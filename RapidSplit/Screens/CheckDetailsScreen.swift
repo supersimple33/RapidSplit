3daad4bbcb1b680f9e274075614e98ee59b6652f
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-10-05 14:11:05 -0400

    feat: Making check details screen

--- RapidSplit/Screens/CheckDetailsScreen.swift
+++ RapidSplit/Screens/CheckDetailsScreen.swift
@@ -10,36 +10,54 @@ import SwiftData
 import MaterialUIKit
 
 struct CheckDetailsScreen: View {
-    @Environment(\.modelContext) private var modelContext
     @Environment(Router.self) private var router
 
     var check: Check
 
     @State private var showDialogSheet = false
     @State private var selectedParticipant: Participant?
+    @State private var selectedTab: TabBarItem = Tab.payout.tabBarItem
 
     @State private var showSnackbar = false
     @State private var snackbarMessage: String = ""
 
-    var body: some View {
-        Container {
-            TotalsTable(check: check, handlePayout: { participant in
-                self.selectedParticipant = participant
-                self.showDialogSheet = true
-            })
-        }
-        .dialogSheet(isPresented: $showDialogSheet) {
-            ParticipantOverview(participant: selectedParticipant) {
-                do {
-                    try self.modelContext.save()
-                } catch let error {
-                    self.snackbarMessage = error.localizedDescription
-                    self.showSnackbar = true
-                }
-                self.showDialogSheet = false
+    fileprivate enum Tab: String {
+        case payout = "Payout"
+        case items = "Items"
+        case participants = "Participants"
+        case assignment = "Assignment"
+
+        var systemImage: String {
+            switch self {
+            case .payout: return "house.fill"
+            case .items: return "list.bullet.rectangle"
+            case .participants: return "person.2"
+            case .assignment: return "mail.and.text.magnifyingglass"
             }
         }
-        .snackbar(isPresented: $showSnackbar, message: snackbarMessage)
+
+        var tabBarItem: TabBarItem {
+            TabBarItem(systemImage: systemImage, titleKey: rawValue)
+        }
+    }
+
+    var body: some View {
+        TabBar(selection: $selectedTab, usesVerticalLayout: true) {
+            PayoutsTab(check: check)
+                .tabBarItem(tab: .payout, selection: $selectedTab)
+            ItemsEditTab(check: check)
+                .tabBarItem(tab: .items, selection: $selectedTab)
+            IdentifyParticipantsScreen(check: check, showContinue: false)
+                .tabBarItem(tab: .participants, selection: $selectedTab)
+            ItemAssignmentScreen(check: check, showContinue: false)
+                .tabBarItem(tab: .assignment, selection: $selectedTab)
+        }
+    }
+}
+
+fileprivate extension View {
+    func tabBarItem(tab: CheckDetailsScreen.Tab, selection: Binding<TabBarItem>) -> some View {
+        return self.tabBarItem(systemImage: tab.systemImage, titleKey: tab.rawValue, selection: selection)
     }
 }
 
