5e9b1a0efef0c4cfda2efed13e28fc12f201af12
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-10-05 13:40:32 -0400

    feat(ui): Adding edit items tab

---
+++ RapidSplit/Components/Tabs/ItemsEditTab.swift
@@ -0,0 +1,28 @@
+//
+//  ItemsEditTab.swift
+//  RapidSplit
+//
+//  Created by Addison Hanrattie on 10/5/25.
+//
+
+import SwiftUI
+import MaterialUIKit
+
+struct ItemsEditTab: View {
+    let check: Check
+
+    var body: some View {
+        Container {
+            ItemsTable(check: check)
+                .floatingActionButton(systemImage: "square.and.pencil", titleKey: "Add New Item") {
+                    check.items.append(
+                        Item(
+                            name: "New Item",
+                            price: 10.0,
+                            forCheck: self.check
+                        )
+                    )
+                }
+        }
+    }
+}
