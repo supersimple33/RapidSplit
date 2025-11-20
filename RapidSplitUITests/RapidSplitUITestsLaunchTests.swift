f7d74ed12f011699ad53351a27e6df55cb427161
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2025-11-20 00:20:50 -0500

    test: setting wait until responsive

--- RapidSplitUITests/RapidSplitUITestsLaunchTests.swift
+++ RapidSplitUITests/RapidSplitUITestsLaunchTests.swift
@@ -18,16 +18,10 @@ final class RapidSplitUITestsLaunchTests: XCTestCase {
     }
 
     @MainActor
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
+    func testLaunchPerformance() throws {
+        // This measures how long it takes to launch your application.
+        measure(metrics: [XCTApplicationLaunchMetric(waitUntilResponsive: true)]) {
+            XCUIApplication().launch()
+        }
     }
 }
